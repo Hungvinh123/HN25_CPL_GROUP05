407e743a1c366f25713cac0cd9be49295d2e9448
oneoutnewbie <nguyenminhhieu342003@gmail.com>
2025-04-10 08:51:06 +0700

    hieu
    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,58 @@
-// Home.js
 import React from "react";
 import "./Home.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Header from "../pages/Header"; // Import Header component
+import Header from "../pages/Header"; 
 
 const Home = () => {
   const [articles, setArticles] = useState([]);
+  const [tags, setTags] = useState([]);
   const [tag, setTag] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [activeFeed, setActiveFeed] = useState("global");
+  const [isLoading, setIsLoading] = useState(false);
+  const limit = 10;
+  const tagLimit = 30;
+
 
   const getArticles = async () => {
+    setIsLoading(true);
     try {
+      const token = localStorage.getItem("token");
+      const baseUrl = "https://node-express-conduit.appspot.com/api";
+      const endpoint = activeFeed === "private" ? "/articles/feed" : "/articles";
+
       const response = await axios.get(
-        `https://node-express-conduit.appspot.com/api/articles?limit=20&offset=0${
-          tag && `&tag=${tag}`
-        }`
+        `${baseUrl}${endpoint}?limit=${limit}&offset=${(page - 1) * 10}${tag && `&tag=${tag}`}`,
+        token && activeFeed === "private"
+          ? {
+              headers: {
+                Authorization: `Token ${token}`,
+              },
+            }
+          : {}
       );
+
       if (response.status === 200) {
         setArticles(response.data.articles);
+        setTotalPages(Math.ceil(response.data.articlesCount / limit));
+      }
+    } catch (e) {
+      console.log(e);
+      setArticles([]);
+      setTotalPages(0);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  const getTags = async () => {
+    try {
+      const response = await axios.get("https://node-express-conduit.appspot.com/api/tags");
+      if (response.status === 200) {
+        setTags(response.data.tags);
       }
     } catch (e) {
       console.log(e);
@@ -25,8 +60,15 @@ const Home = () => {
   };
 
   useEffect(() => {
+    getTags();
+  }, []);
+
+  useEffect(() => {
+    setTotalPages(0);
     getArticles();
-  }, [tag]);
+  }, [tag, page, activeFeed]);
+
+  console.log(totalPages);
 
   const handleFavorite = async (slug) => {
     try {
@@ -64,67 +106,111 @@ const Home = () => {
 
   return (
     <>
-      <Header /> {/* Sử dụng Header component */}
+    <Header />
       <div className="banner">
         <h1>conduit</h1>
         <p>A place to share your knowledge.</p>
       </div>
       <div className="home-container">
         <div className="feed-navigation">
-          <button className="feed-btn">Your Feed</button>
-          <button className="feed-btn active">Global Feed</button>
+          <button
+            className={`feed-btn ${activeFeed === "private" ? "active" : ""}`}
+            onClick={() => {
+              setActiveFeed("private");
+              setTag("");
+              setPage(1);
+            }}
+          >
+            Your Feed
+          </button>
+          <button
+            className={`feed-btn ${activeFeed === "global" ? "active" : ""}`}
+            onClick={() => {
+              setActiveFeed("global");
+              setTag("");
+              setPage(1);
+            }}
+          >
+            Global Feed
+          </button>
+          {tag && <button className="feed-btn active">{tag}</button>}
         </div>
-
         <div className="content-wrapper">
           <main className="articles-section">
-            {articles.length > 0
-              ? articles?.map((article) => (
-                  <article className="article-card" key={article?.slug}>
-                    <div className="article-meta">
-                      <img src={article?.author?.image} alt="Author" className="author-image" />
-                      <div className="author-info">
-                        <p>{article?.author?.username}</p>
-                        <span>
-                          {new Date(article?.createdAt).toLocaleDateString("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                          })}
-                        </span>
-                      </div>
-                      <div
-                        className={`article-favorite ${article?.favorited ? "favorited  " : ""}`}
-                        onClick={() => handleFavorite(article?.slug)}
-                      >
-                        {article?.favoritesCount}
-                      </div>
+            {isLoading ? (
+              <div>Đang tải bài viết...</div>
+            ) : articles.length > 0 ? (
+              articles?.map((article) => (
+                <article className="article-card" key={article?.slug}>
+                  <div className="article-meta">
+                    <img src={article?.author?.image} alt="Author" className="author-image" />
+                    <div className="author-info">
+                      <p>{article?.author?.username}</p>
+                      <span>
+                        {new Date(article?.createdAt).toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })}
+                      </span>
                     </div>
-                    <div className="article-content">
-                      <h2>{article?.title}</h2>
-                      <p>{article?.description}</p>
-                      <a href="#" className="read-more">
-                        Read more...
-                      </a>
+                    <div
+                      className={`article-favorite ${article?.favorited ? "favorited  " : ""}`}
+                      onClick={() => handleFavorite(article?.slug)}
+                    >
+                      {article?.favoritesCount}
                     </div>
-                  </article>
-                ))
-              : "Đang tải dữ liệu"}
+                  </div>
+                  <div className="article-content">
+                    <h2>{article?.title}</h2>
+                    <p>{article?.description}</p>
+                    <a href="#" className="read-more">
+                      Read more...
+                    </a>
+                  </div>
+                </article>
+              ))
+            ) : (
+              "Không có bài viết nào"
+            )}
           </main>
 
           <aside className="sidebar">
             <div className="popular-tags">
               <h3>Popular Tags</h3>
               <div className="tags-container">
-                <span className="tag">programming</span>
-                <span className="tag">javascript</span>
-                <span className="tag">angularjs</span>
-                <span className="tag">react</span>
-                <span className="tag">mean</span>
-                <span className="tag">node</span>
-                <span className="tag">rails</span>
+                {tags.length > 0 &&
+                  tags?.map(
+                    (tag, index) =>
+                      index <= tagLimit && (
+                        <span
+                          className="tag"
+                          onClick={() => {
+                            setTag(tag);
+                            setPage(1);
+                            setActiveFeed("");
+                          }}
+                          key={index}
+                        >
+                          {tag}
+                        </span>
+                      )
+                  )}
               </div>
             </div>
           </aside>
+        </div>
+
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              className={`pagination-btn ${page === i + 1 ? "active" : ""}`}
+            >
+              {i + 1}
+            </button>
+          ))}
         </div>
       </div>
     </>
